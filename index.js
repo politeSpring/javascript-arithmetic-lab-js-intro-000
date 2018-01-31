c17db1aab7380b9cf59c348ac17a7a8333de1745
Sugar 2017 <politespring@gmail.com>
2018-01-31 13:11:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,32 @@ function add(a,b) {
   number=a+b;
   return number;
 }
+function subtract(a,b){
+  number=a-b;
+  return number;
+}
+function multiply(a,b){
+  number=a*b;
+  return number;
+}
+function divide(a,b){
+  number=a/b;
+  return number;
+}
+function inc(a){
+ number=a+1;
+ return number;
+  
+}
+function dec(a){
+  number=a-1;
+  return number;
+}
+function makeInt(a){
+  number= parseInt(a,10);
+  return number;
+}
+function preserveDecimal(a){
+  number=preserveDecimal(a,10);
+  return number;
+}
